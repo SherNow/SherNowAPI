84e638509e79ffc29465276e94e1ef4d58f4fb62
Simon Lacoursiere <lacoursieresimon@gmail.com>
2015-12-03 16:43:33 -0500

    Added façade pattern to the garbage controller.

--- modules/garbage/server/controllers/garbage.server.controller.js
+++ modules/garbage/server/controllers/garbage.server.controller.js
@@ -33,25 +33,68 @@ exports.list = function (req, res) {
     });
 };
 
-/**
- * Garbage middleware
- */
-exports.garbageByPostalCode = function (req, res, next, postalCode) {
-    getGeolocationFromAddress(postalCode)
-        .then(function(geoLocation) {
-            Garbage.findAll().then(function(features) {
 
-                // Possible cellect day resolution
+
+var GeolocalisationGetter = function(){
+    this.get = function(postalCode) {
+        return new Promise(function(resolve, reject) {
+            console.log('http://maps.googleapis.com/maps/api/geocode/json?address=' + postalCode);
+            request('http://maps.googleapis.com/maps/api/geocode/json?address=' + postalCode,
+                function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        var res = JSON.parse(body);
+                        resolve(res.results[0].geometry.location);
+                    } else {
+                        reject(error);
+                    }
+                });
+        });
+    };
+};
+
+var CollectZoneGetter = function(){
+    this.get = function(geoLocation, features){
+        var zone = null;
+        console.log(geoLocation);
+        
+        _.each(features, function (polygone) {
+        if (geojsonUtils.pointInPolygon({"type":"Point","coordinates":[geoLocation.lng, geoLocation.lat]},
+                {"type":polygone.feature.geometry.type, "coordinates": polygone.feature.geometry.coordinates })) {
+            zone = polygone;
+        }
+    });
+
+        return zone;
+    };
+};
+
+var GarbageCollectDay = function(){
+};
+
+GarbageCollectDay.prototype =  {
+    getCollectDay : function(postalCode, res){
+        
+        var geoGetter = new GeolocalisationGetter();
+        var zoneGetter = new CollectZoneGetter();
+        
+        geoGetter.get(postalCode).then(function (geoLocation) {
+            Garbage.findAll().then(function (features) {
+
+                var collect = {};
+                var collectZone = zoneGetter.get(geoLocation, features);
+                
+                if (collectZone === null){
+                    return res.json({collectDay : "Code postal erroné" });
+                }
+                
+                // Possible collect day resolution
                 var collectDays = [
                     'JOURCUEILLETTE1_resolved',
                     'JOURCUEILLETTE2_resolved',
                     'JOURCUEILLETTE3_resolved',
                 ];
-
-                var collect = {};
-                var collectZone = getCollectZone(geoLocation, features);
-
-                _.each(collectDays, function(collectDay) {
+                
+                _.each(collectDays, function (collectDay) {
                     if (collectZone.feature.properties[collectDay]) {
                         collect = {
                             _id: collectZone._id,
@@ -67,47 +110,18 @@ exports.garbageByPostalCode = function (req, res, next, postalCode) {
                     message: errorHandler.getErrorMessage(err)
                 });
             });
-        },function(err) {
+        }, function (err) {
             console.log("error");
             return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
             });
         });
+    }
 };
 
 /**
- * Return Collect Zone
- * @param geoLocation
- * @param features
+ * Garbage middleware
  */
-function getCollectZone(geoLocation, features) {
-    var zone = null;
-    _.each(features, function (polygone) {
-        if (geojsonUtils.pointInPolygon({"type":"Point","coordinates":[geoLocation.lng, geoLocation.lat]},
-                {"type":polygone.feature.geometry.type, "coordinates": polygone.feature.geometry.coordinates })) {
-            zone = polygone;
-        }
-    });
-
-    return zone;
-}
-
-/**
- * Return Geolocation Json object contaning latitude and longitude based on an address
- * Documentaion : https://developers.google.com/maps/documentation/geocoding/
- * @param address
- * @returns {Promise} {lat: number, lng: number}
- */
-function getGeolocationFromAddress(address) {
-    return new Promise(function(resolve, reject) {
-        request('http://maps.googleapis.com/maps/api/geocode/json?address=' + address,
-            function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var res = JSON.parse(body);
-                    resolve(res.results[0].geometry.location);
-                } else {
-                    reject(error);
-                }
-            });
-    });
-}
+exports.garbageByPostalCode = function (req, res, next, postalCode) {
+    new GarbageCollectDay().getCollectDay(postalCode, res);
+};
